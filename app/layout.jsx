f5502dd9bf0e6459e4fd246d512469ab13e347e7
Yeshwant Thota <thotayeshwant@gmail.com>
2023-08-20 11:42:32 +0530

    Navbar and authentication done

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
-
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
@@ -8,10 +9,15 @@ export const metadata = {
 const RootLayout = ({ children }) => (
   <html lang="en">
     <body>
-      <div className="main">
-        <div className="gradient" />
-      </div>
-      <main className="app">{children}</main>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
     </body>
   </html>
 );
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => (
   <section className="w-full flex-center flex-col">
     <h1 className="head_text text-center">
@@ -9,6 +11,7 @@ const Home = () => (
       Promptopia is an open-source AI prompting tool for modern world to
       discover, create and share creative prompts
     </p>
+    <Feed />
   </section>
 );
 
